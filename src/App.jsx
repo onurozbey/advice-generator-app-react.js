745d79bd9b47b12ffc92b0761a317f5f623ac8c8
onurozbey <onurozbey@outlook.com>
2022-08-30 16:46:12 +0300

    component structure has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-import pauseDesktop from "./images/pattern-divider-desktop.svg";
-import pauseMobile from "./images/pattern-divider-mobile.svg";
-import dice from "./images/icon-dice.svg";
+// Components
+import Main from "./components/Main";
 
 function App() {
-  const [advice, setAdvice] = useState([]);
-
-  async function fetchAdvice() {
-    try {
-      const data = await axios.get("https://api.adviceslip.com/advice");
-      // console.log(data.data.slip);
-      setAdvice(data.data.slip);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchAdvice();
-  }, []);
-
   return (
-    <div className="container">
-      <h1>Advice #{advice.id}</h1>
-      <p>{advice.advice}</p>
-      <picture>
-        <source media="(min-width: 768px)" srcSet={pauseDesktop} />
-        <img src={pauseMobile} alt="" />
-      </picture>
-
-      <div>
-        <button onClick={fetchAdvice}>
-          <img src={dice} alt="" />
-        </button>
-      </div>
+    <div>
+      <Main />
     </div>
   );
 }
